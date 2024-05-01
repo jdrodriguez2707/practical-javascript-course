d90f9c9d9a8b142d1494f7d762df6dc4d65cad81
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-01 15:56:24 -0500

    Reading HTML from JavaScript

--- DOM/main.js
+++ DOM/main.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const title = document.querySelector("h1");
+const paragraphs = document.querySelectorAll("p");
+// const paragraphWithClass = document.querySelector(".paragraph");
+const paragraphWithClass = document.getElementsByClassName("paragraph");
+// const paragraphWithClass = document.querySelector("#paragraph");
+const paragraphWithId = document.getElementById("paragraph");
+const inputText = document.querySelector("input");
+
+console.log(title);
+console.log(inputText.value);
+
+console.log({
+  title,
+  paragraphs,
+  paragraphWithClass,
+  paragraphWithId,
+  inputText
+});
