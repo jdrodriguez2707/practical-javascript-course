835a63f7390f587eab432ffd4a56057af832005e
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-07 16:47:51 -0500

    Events in JS: Interacting with users

--- DOM/main.js
+++ DOM/main.js
@@ -1,28 +1,10 @@
-const title = document.querySelector("h1");
-// title.innerHTML = 'God is good <br> All the time'
-title.innerText = "God is good all the time";
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const button = document.querySelector("#calculate-button");
+const resultParagraph = document.querySelector("#result");
 
-const paragraph = document.querySelector("p");
-console.log(paragraph.getAttribute("attribute"));
-paragraph.setAttribute("attribute", "Faith");
-console.log(paragraph.getAttribute("attribute"));
-
-const paragraphWithClass = document.querySelector(".paragraph");
-paragraphWithClass.classList.add("new", "hello");
-paragraphWithClass.classList.remove("new", "hello");
-console.log(paragraphWithClass.classList.contains("paragraph"));
-
-const input = document.querySelector("input");
-input.value = "1234";
-
-const image = document.createElement("img");
-console.log(image);
-image.setAttribute(
-  "src",
-  "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-);
-const paragraphWithId = document.querySelector("#paragraph");
-// paragraphWithId.replaceWith(image)
-paragraphWithId.append(image);
-
-// paragraphWithId.innerHTML = `<img src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" title="A cute dog">`;
+function addNumbers() {
+  // const result = parseInt(input1.value) + parseInt(input2.value);
+  const result = +input1.value + +input2.value;
+  resultParagraph.innerText = "The result is: " + result;
+}
