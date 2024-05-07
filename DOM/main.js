73a487c482e034a9455c6fd9322ee78c3b107716
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-07 18:56:35 -0500

    addEventListener method

--- DOM/main.js
+++ DOM/main.js
@@ -4,9 +4,17 @@ const multiplicationRadio = document.querySelector("#multiplication");
 const divisionRadio = document.querySelector("#division");
 const input1 = document.querySelector("#input1");
 const input2 = document.querySelector("#input2");
+const button = document.querySelector("#calculate-button");
 const resultParagraph = document.querySelector("#result");
+const form = document.querySelector("#form");
 
-function calculate() {
+// button.addEventListener("click", calculate);
+form.addEventListener("submit", calculate);
+
+function calculate(event) {
+  console.log({ event });
+  event.preventDefault();
+
   let result = 0;
 
   if (additionRadio.checked) {
@@ -19,5 +27,20 @@ function calculate() {
     result = +input1.value / +input2.value;
   }
 
-  resultParagraph.innerText = "The result is: " + result;
+  if (result) {
+    resultParagraph.innerText = "The result is: " + result;
+  } else {
+    return;
+  }
+
+  // let confirmation = confirm(
+  //   "Do you want to send this response to the teacher?"
+  // );
+
+  // if (confirmation) {
+  //   console.log("Form sent");
+  //   form.submit();
+  // } else {
+  //   console.log("Form not sent");
+  // }
 }
