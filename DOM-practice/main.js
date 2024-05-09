3ddf50ef94a36d1c61fe33f801c616a2bbd8fa46
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-09 14:57:57 -0500

    AddEventListener (practice)

--- DOM-practice/main.js
+++ DOM-practice/main.js
@@ -2,7 +2,22 @@ const input1 = document.querySelector("#input1");
 const input2 = document.querySelector("#input2");
 const additionButton = document.querySelector("#addition-button");
 const result = document.getElementById("result");
+const form = document.getElementById("form");
 
-function addNumber() {
+form.addEventListener("submit", (event) => {
+  console.log({ event });
+  event.preventDefault();
   result.innerText = `The result is: ${+input1.value + +input2.value}`;
-}
+
+  const newButton = document.createElement("button");
+  newButton.innerText = "Send response";
+  newButton.addEventListener("click", () => {
+    alert("Response sent");
+  });
+
+  result.append(newButton);
+});
+
+// function addNumber() {
+//   result.innerText = `The result is: ${+input1.value + +input2.value}`;
+// }
